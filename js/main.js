b134c604c975852ca418190e2b21ecbae0c86076
unknown <jrmcgee@asu.edu>
2021-04-25 13:32:57 +0900

    First commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function solution(str){
+    var splitString = str.split(""); // var splitString = "hello".split("");
+      // ["h", "e", "l", "l", "o"]
+    var reverseArray = splitString.reverse(); //var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+      // ["o", "l", "l", "e", "h"]
+   
+    var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+      // "olleh"
+    return joinArray;
+    
+  }
